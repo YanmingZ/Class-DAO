bdae1f3635deb36d4ee88611da20bd645a1944fa
YanmingZ <115550398+YanmingZ@users.noreply.github.com>
2023-04-23 12:09:38 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config({ path: ".env" });
 require("@nomicfoundation/hardhat-toolbox");
-
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  
+  solidity: "0.8.17",
+  networks:{
+    alfajores: {
+    url: "https://alfajores-forno.celo-testnet.org",
+    accounts: [process.env.PRIVATE_KEY],
+    chainId: 44787,
+    }
+   }
 };
